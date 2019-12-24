8ef20951c5ad3aa9863effc04e5b778873661845
Adebayo Olaonipekun <adebayool@armpension.com>
2019-12-24 01:03:25 +0100

    fresh start

--- Admin/inc/view/footer.php
+++
@@ -1,6 +0,0 @@
-      <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-          <b>Version</b> 2.2.0
-        </div>
-        <strong>Copyright &copy; 2014-<?php echo date('Y'); ?> <a href="http://adebayopeter.com">Adebayo Peter Olaonipekun</a>.</strong> All rights reserved.
-      </footer>
